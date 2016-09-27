a85ae621fca9dec3329f02eff5c004f2a03502a0
Carter hong <carter@qeeka.com>
2016-09-27 17:45:52 +0800

    产品预定页 initjs

---
+++ assets/js/tjhjs.js
@@ -0,0 +1,35 @@
+/*
+ * 依赖ZeptoJs
+ * 组件是：页面浮动菜单
+*/
+
+$(function () {
+    'use strict';
+
+    /*===========================
+        TJHJs
+    ===========================*/
+
+    //页面浮动菜单
+    $(document).on("pageInit", ".page", function(e, id, page){
+        var mark = "<div class='page-mark'></div>";
+        $(".page-nav").on("click", ".menu", function(){
+            if($(".page-mark").length == 0){
+                $(".page").append(mark);
+            }
+            if(!$(this).closest("ul").hasClass("active")){
+                $(this).closest("ul").addClass("active");
+            }
+            else{
+                $(this).closest("ul").removeClass("active");
+                $(".page-mark").remove();
+            }
+        });
+        $(".page").on("click", ".page-mark", function(){
+            $(this).remove();
+            $(".page-nav").find("ul").removeClass("active");
+        });
+    });
+    $.init();
+});
+
